5e59051c46609bd48ea753d892a1e285fa2b36ff
Алексей <balko-aleksey@yandex.ru>
2023-02-11 22:19:34 +0700

    tasks 1-3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        //task4();
+        //task5();
+        //task6();
+        //task7();
+        //task8();
     }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int amountOfSaving = 0;
+        int stash = 15_000;
+        int desiredSavings = 2_459_000;
+        int months = 0;
+        while (amountOfSaving <= desiredSavings) {
+            amountOfSaving = amountOfSaving + stash;
+            System.out.println("Месяц " + months + " сумма накоплений равна " + amountOfSaving);
+            months++;
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(" " + i);
+            i++;
+        }
+        System.out.println();
+        for (int number = 10; number >= 1; number--) {
+            System.out.print(" " + number);
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int populationOfTheContry = 12_000_000;
+        int years = 10;
+        int birthRate = 17;
+        int mortality = 8;
+        for (int i = 1; i <= years; i++) {
+            populationOfTheContry += (populationOfTheContry / 1000 * (birthRate - mortality));
+            System.out.println("Год " + i + " численность населения составляет " + populationOfTheContry);
+        }
+    }
+
 }
+
