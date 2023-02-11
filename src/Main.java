a4ee4037825d6b23c36e5a943c62424ee49873c6
Алексей <balko-aleksey@yandex.ru>
2023-02-12 00:08:34 +0700

    tasks 4-8 completed

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
         task1();
         task2();
         task3();
-        //task4();
-        //task5();
-        //task6();
-        //task7();
-        //task8();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
     }
 
     public static void task1() {
@@ -49,5 +49,67 @@ public class Main {
         }
     }
 
+    public static void task4() {
+        System.out.println("Задача 4");
+        double depositAmount = 15_000;
+        int totalMoney = 12_000_000;
+        double rate = 0.07;
+        int currentMonths = 1;
+        while (depositAmount <= totalMoney) {
+            depositAmount *= (1 + rate);
+            System.out.println("Месяц " + currentMonths + " накопления " + depositAmount);
+            currentMonths++;
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+        double depositAmount = 15_000;
+        int totalMoney = 12_000_000;
+        double ratePerMonth = 0.07;
+        int currentMonths = 1;
+        while (depositAmount <= totalMoney) {
+            depositAmount *= (1 + ratePerMonth);
+            if (currentMonths % 6 == 0) {
+                System.out.println("Месяц " + currentMonths + " накопления " + depositAmount);
+            }
+            currentMonths++;
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6");
+        double depositAmount = 15_000;
+        double ratePerMonth = 0.07;
+        int years = 9;
+
+        for (int currentMonths = 1; currentMonths < years * 12; currentMonths++) {
+            depositAmount *= (1 + ratePerMonth);
+            if (currentMonths % 6 == 0) {
+                System.out.println("Месяц " + currentMonths + " накопления " + depositAmount);
+            }
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7");
+        int friday = 5;
+        int month = 31;
+        for (int i = friday; i < month; i = i + 7) {
+            System.out.println("Сегодня пятница, " + i + " -е число. Необходимо подготовить отчет");
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8");
+        int currentYear = 2023;
+        int recentYears = currentYear - 200;
+        int commingYear = currentYear + 100;
+        for (int i = recentYears; i <= commingYear; i++) {
+            if (i % 79 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
 
